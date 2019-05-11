03e9391a4e276866bee284f640fb621161490823
samuelroland <samuel.roland@cpnv.ch>
2019-05-11 19:33:28 +0200

    suite

---
+++ 03-Deductions/03-Deductions/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace _03_Deductions
+{
+    partial class frmDeductions
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRevenu = new System.Windows.Forms.Label();
+            this.lblCoefficient = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lblRevenu
+            // 
+            this.lblRevenu.AutoSize = true;
+            this.lblRevenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRevenu.Location = new System.Drawing.Point(24, 27);
+            this.lblRevenu.Name = "lblRevenu";
+            this.lblRevenu.Size = new System.Drawing.Size(134, 18);
+            this.lblRevenu.TabIndex = 0;
+            this.lblRevenu.Text = "Revenu annuel brut";
+            // 
+            // lblCoefficient
+            // 
+            this.lblCoefficient.AutoSize = true;
+            this.lblCoefficient.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCoefficient.Location = new System.Drawing.Point(24, 79);
+            this.lblCoefficient.Name = "lblCoefficient";
+            this.lblCoefficient.Size = new System.Drawing.Size(46, 18);
+            this.lblCoefficient.TabIndex = 1;
+            this.lblCoefficient.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(93, 318);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.checkBox.Location = new System.Drawing.Point(96, 145);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(101, 22);
+            this.checkBox.TabIndex = 3;
+            this.checkBox.Text = "checkBox1";
+            this.checkBox.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.checkBox2.Location = new System.Drawing.Point(96, 173);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(101, 22);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.checkBox3.Location = new System.Drawing.Point(96, 201);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(101, 22);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // frmDeductions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 364);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblCoefficient);
+            this.Controls.Add(this.lblRevenu);
+            this.Name = "frmDeductions";
+            this.Text = "Déductions";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRevenu;
+        private System.Windows.Forms.Label lblCoefficient;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
