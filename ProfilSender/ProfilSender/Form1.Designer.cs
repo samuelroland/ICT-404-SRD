69a70316ba08008134d9a789f66ad4fb097bf52f
samuelroland <samuel.roland@cpnv.ch>
2019-05-20 20:35:14 +0200

    ProfilSender créé et WIP
    Exo trombinoscope avec Fabien. Fenetre de choix des options: présence, humeur et commentaire.

---
+++ ProfilSender/ProfilSender/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ProfilSender
+{
+    partial class frmProfilSender
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkPresent = new System.Windows.Forms.CheckBox();
+            this.optContent = new System.Windows.Forms.RadioButton();
+            this.optCool = new System.Windows.Forms.RadioButton();
+            this.optNerveux = new System.Windows.Forms.RadioButton();
+            this.optPasContent = new System.Windows.Forms.RadioButton();
+            this.optAngelique = new System.Windows.Forms.RadioButton();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // chkPresent
+            // 
+            this.chkPresent.AutoSize = true;
+            this.chkPresent.Location = new System.Drawing.Point(25, 12);
+            this.chkPresent.Name = "chkPresent";
+            this.chkPresent.Size = new System.Drawing.Size(62, 17);
+            this.chkPresent.TabIndex = 0;
+            this.chkPresent.Text = "Présent";
+            this.chkPresent.UseVisualStyleBackColor = true;
+            this.chkPresent.CheckedChanged += new System.EventHandler(this.ChkPresent_CheckedChanged);
+            // 
+            // optContent
+            // 
+            this.optContent.AutoSize = true;
+            this.optContent.Location = new System.Drawing.Point(119, 12);
+            this.optContent.Name = "optContent";
+            this.optContent.Size = new System.Drawing.Size(62, 17);
+            this.optContent.TabIndex = 1;
+            this.optContent.Text = "Content";
+            this.optContent.UseVisualStyleBackColor = true;
+            this.optContent.CheckedChanged += new System.EventHandler(this.OptContent_CheckedChanged);
+            // 
+            // optCool
+            // 
+            this.optCool.AutoSize = true;
+            this.optCool.Location = new System.Drawing.Point(119, 41);
+            this.optCool.Name = "optCool";
+            this.optCool.Size = new System.Drawing.Size(46, 17);
+            this.optCool.TabIndex = 2;
+            this.optCool.Text = "Cool";
+            this.optCool.UseVisualStyleBackColor = true;
+            this.optCool.CheckedChanged += new System.EventHandler(this.OptCool_CheckedChanged);
+            // 
+            // optNerveux
+            // 
+            this.optNerveux.AutoSize = true;
+            this.optNerveux.Location = new System.Drawing.Point(119, 70);
+            this.optNerveux.Name = "optNerveux";
+            this.optNerveux.Size = new System.Drawing.Size(65, 17);
+            this.optNerveux.TabIndex = 3;
+            this.optNerveux.Text = "Nerveux";
+            this.optNerveux.UseVisualStyleBackColor = true;
+            this.optNerveux.CheckedChanged += new System.EventHandler(this.OptNerveux_CheckedChanged);
+            // 
+            // optPasContent
+            // 
+            this.optPasContent.AutoSize = true;
+            this.optPasContent.Location = new System.Drawing.Point(119, 99);
+            this.optPasContent.Name = "optPasContent";
+            this.optPasContent.Size = new System.Drawing.Size(82, 17);
+            this.optPasContent.TabIndex = 4;
+            this.optPasContent.Text = "Pas content";
+            this.optPasContent.UseVisualStyleBackColor = true;
+            this.optPasContent.CheckedChanged += new System.EventHandler(this.OptPasContent_CheckedChanged);
+            // 
+            // optAngelique
+            // 
+            this.optAngelique.AutoSize = true;
+            this.optAngelique.Location = new System.Drawing.Point(119, 128);
+            this.optAngelique.Name = "optAngelique";
+            this.optAngelique.Size = new System.Drawing.Size(72, 17);
+            this.optAngelique.TabIndex = 5;
+            this.optAngelique.Text = "Angélique";
+            this.optAngelique.UseVisualStyleBackColor = true;
+            this.optAngelique.CheckedChanged += new System.EventHandler(this.OptAngelique_CheckedChanged);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(22, 165);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(50, 13);
+            this.lblMessage.TabIndex = 6;
+            this.lblMessage.Text = "Message";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(25, 181);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(233, 66);
+            this.txtMessage.TabIndex = 7;
+            this.txtMessage.Validated += new System.EventHandler(this.TxtMessage_Validated);
+            // 
+            // frmProfilSender
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 259);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.optAngelique);
+            this.Controls.Add(this.optPasContent);
+            this.Controls.Add(this.optNerveux);
+            this.Controls.Add(this.optCool);
+            this.Controls.Add(this.optContent);
+            this.Controls.Add(this.chkPresent);
+            this.Name = "frmProfilSender";
+            this.Text = "Username";
+            this.Load += new System.EventHandler(this.FrmProfilSender_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkPresent;
+        private System.Windows.Forms.RadioButton optContent;
+        private System.Windows.Forms.RadioButton optCool;
+        private System.Windows.Forms.RadioButton optNerveux;
+        private System.Windows.Forms.RadioButton optPasContent;
+        private System.Windows.Forms.RadioButton optAngelique;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtMessage;
+    }
+}
+
---
+++ ProfilSender/ProfilSender/Form1.cs
@@ -0,0 +1,172 @@
+﻿//Projet: Trombinoscope Samuel + Fabien. Dernier exo de Moodle.
+//Auteur: Samuel Roland
+//Date: mai 2019.
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; //permet d'écrire et de lire dans un fichier. mais pas que.   
+
+namespace ProfilSender
+{
+    public partial class frmProfilSender : Form
+    {
+        int dejaecrit;  //définit si on a deja écrit un texte. permet d'envoyer le profil que quand tout rempli.
+
+        public frmProfilSender()
+        {
+            InitializeComponent();
+        }
+        private void ecriredansunfichier()
+        {
+            //variables pour les choix, elles seront directement écrites dans le fichier.
+            int presence;
+            int humeur = 0;     // de 1 à 5 dans l'ordre: Content, Cool, Nerveux, Pas content, Angélique
+            string commentaire;
+
+            //Prendre les données:
+
+            //Présence:
+            if (chkPresent.Checked == false)
+            {
+                presence = 0;
+            }
+            else
+            {
+                presence = 1;
+            }
+            //Humeur:
+            if (optContent.Checked == true)
+            {
+                humeur = 1;
+            }
+            if (optCool.Checked == true)
+            {
+                humeur = 2;
+            }
+            if (optNerveux.Checked == true)
+            {
+                humeur = 3;
+            }
+            if (optPasContent.Checked == true)
+            {
+                humeur = 4;
+            }
+            if (optAngelique.Checked == true)
+            {
+                humeur = 5;
+            }
+
+
+            //Texte commentaire:
+            commentaire = txtMessage.Text;
+
+
+
+            //Exemple pour lire dans un fichier:
+
+            // Essai de lecture d'un fichier:
+            // string ligne = null;
+            // StreamReader fluxInfos = null;
+            // 
+            // 
+            // using (fluxInfos = new StreamReader("infos.txt"))
+            // {
+            //     
+            //     do
+            //     {
+            //         ligne = fluxInfos.ReadLine();
+            //         txtMessage.Text += ligne + "\r\n";
+            //     } while (ligne != null);
+            // }
+
+            //Ecriture dans un fichier:
+            string ligne = null;
+            StreamWriter fluxinfos = null;
+            try
+            {
+                using (fluxinfos = new StreamWriter("infos.txt"))   // si on créé un fichier il écrase l'ancien du meme nom.
+                {
+                    //Ecrire les 3 infos:
+                    fluxinfos.WriteLine(presence);
+                    fluxinfos.WriteLine(humeur);
+                    fluxinfos.WriteLine(commentaire);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Erreur avec le fichier !");
+            }
+
+        }
+        private void FrmProfilSender_Load(object sender, EventArgs e)
+        {
+            string username = Environment.UserName;
+            MessageBox.Show(username);
+            this.Text = username;
+        }
+
+        private void ChkPresent_CheckedChanged(object sender, EventArgs e)
+        {
+            if (dejaecrit == 1)
+            {
+                ecriredansunfichier();
+            }
+        }
+
+        private void OptContent_CheckedChanged(object sender, EventArgs e)
+        {
+            if (dejaecrit == 1)
+            {
+                ecriredansunfichier();
+            }
+        }
+
+        private void OptCool_CheckedChanged(object sender, EventArgs e)
+        {
+            if (dejaecrit == 1)
+            {
+                ecriredansunfichier();
+            }
+        }
+
+        private void OptNerveux_CheckedChanged(object sender, EventArgs e)
+        {
+            if (dejaecrit == 1)
+            {
+                ecriredansunfichier();
+            }
+        }
+
+        private void OptPasContent_CheckedChanged(object sender, EventArgs e)
+        {
+            if (dejaecrit == 1)
+            {
+                ecriredansunfichier();
+            }
+        }
+
+        private void OptAngelique_CheckedChanged(object sender, EventArgs e)
+        {
+            if (dejaecrit == 1)
+            {
+                ecriredansunfichier();
+            }
+
+        }
+
+        private void TxtMessage_Validated(object sender, EventArgs e)   //Au moment de la validationd du texte. pas pendant qu'on écrit.
+        {
+            dejaecrit = 1;
+            ecriredansunfichier();
+        }
+    }
+
+}
