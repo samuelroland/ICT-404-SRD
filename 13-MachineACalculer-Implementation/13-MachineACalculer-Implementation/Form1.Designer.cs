5abc1b935f968db9d763b4d9f6cc5b9e2cd3f7ec
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2019-06-13 09:36:32 +0200

    13-MachineACalculer WIP implémentation

---
+++ 13-MachineACalculer-Implementation/13-MachineACalculer-Implementation/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace _13_MachineACalculer_Implementation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMachineACalculerInfo = new System.Windows.Forms.Label();
+            this.cmd1 = new System.Windows.Forms.Button();
+            this.cmd2 = new System.Windows.Forms.Button();
+            this.cmd3 = new System.Windows.Forms.Button();
+            this.cmd4 = new System.Windows.Forms.Button();
+            this.cmd5 = new System.Windows.Forms.Button();
+            this.cmd6 = new System.Windows.Forms.Button();
+            this.cmd7 = new System.Windows.Forms.Button();
+            this.cmd8 = new System.Windows.Forms.Button();
+            this.cmd9 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblNbEntier1 = new System.Windows.Forms.Label();
+            this.lblNbEntier2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMachineACalculerInfo
+            // 
+            this.lblMachineACalculerInfo.AutoSize = true;
+            this.lblMachineACalculerInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMachineACalculerInfo.Location = new System.Drawing.Point(13, 9);
+            this.lblMachineACalculerInfo.Name = "lblMachineACalculerInfo";
+            this.lblMachineACalculerInfo.Size = new System.Drawing.Size(193, 25);
+            this.lblMachineACalculerInfo.TabIndex = 2;
+            this.lblMachineACalculerInfo.Text = "Machine à calculer";
+            // 
+            // cmd1
+            // 
+            this.cmd1.Location = new System.Drawing.Point(18, 194);
+            this.cmd1.Name = "cmd1";
+            this.cmd1.Size = new System.Drawing.Size(43, 36);
+            this.cmd1.TabIndex = 3;
+            this.cmd1.Text = "1";
+            this.cmd1.UseVisualStyleBackColor = true;
+            this.cmd1.Click += new System.EventHandler(this.number_Click);
+            // 
+            // cmd2
+            // 
+            this.cmd2.Location = new System.Drawing.Point(69, 194);
+            this.cmd2.Name = "cmd2";
+            this.cmd2.Size = new System.Drawing.Size(43, 36);
+            this.cmd2.TabIndex = 4;
+            this.cmd2.Text = "2";
+            this.cmd2.UseVisualStyleBackColor = true;
+            this.cmd2.Click += new System.EventHandler(this.number_Click);
+            // 
+            // cmd3
+            // 
+            this.cmd3.Location = new System.Drawing.Point(118, 194);
+            this.cmd3.Name = "cmd3";
+            this.cmd3.Size = new System.Drawing.Size(43, 36);
+            this.cmd3.TabIndex = 5;
+            this.cmd3.Text = "3";
+            this.cmd3.UseVisualStyleBackColor = true;
+            this.cmd3.Click += new System.EventHandler(this.number_Click);
+            // 
+            // cmd4
+            // 
+            this.cmd4.Location = new System.Drawing.Point(18, 236);
+            this.cmd4.Name = "cmd4";
+            this.cmd4.Size = new System.Drawing.Size(43, 36);
+            this.cmd4.TabIndex = 6;
+            this.cmd4.Text = "4";
+            this.cmd4.UseVisualStyleBackColor = true;
+            this.cmd4.Click += new System.EventHandler(this.number_Click);
+            // 
+            // cmd5
+            // 
+            this.cmd5.Location = new System.Drawing.Point(69, 236);
+            this.cmd5.Name = "cmd5";
+            this.cmd5.Size = new System.Drawing.Size(43, 36);
+            this.cmd5.TabIndex = 7;
+            this.cmd5.Text = "5";
+            this.cmd5.UseVisualStyleBackColor = true;
+            this.cmd5.Click += new System.EventHandler(this.number_Click);
+            // 
+            // cmd6
+            // 
+            this.cmd6.Location = new System.Drawing.Point(118, 236);
+            this.cmd6.Name = "cmd6";
+            this.cmd6.Size = new System.Drawing.Size(43, 36);
+            this.cmd6.TabIndex = 8;
+            this.cmd6.Text = "6";
+            this.cmd6.UseVisualStyleBackColor = true;
+            this.cmd6.Click += new System.EventHandler(this.number_Click);
+            // 
+            // cmd7
+            // 
+            this.cmd7.Location = new System.Drawing.Point(18, 278);
+            this.cmd7.Name = "cmd7";
+            this.cmd7.Size = new System.Drawing.Size(43, 36);
+            this.cmd7.TabIndex = 9;
+            this.cmd7.Text = "7";
+            this.cmd7.UseVisualStyleBackColor = true;
+            // 
+            // cmd8
+            // 
+            this.cmd8.Location = new System.Drawing.Point(69, 278);
+            this.cmd8.Name = "cmd8";
+            this.cmd8.Size = new System.Drawing.Size(43, 36);
+            this.cmd8.TabIndex = 10;
+            this.cmd8.Text = "8";
+            this.cmd8.UseVisualStyleBackColor = true;
+            // 
+            // cmd9
+            // 
+            this.cmd9.Location = new System.Drawing.Point(118, 278);
+            this.cmd9.Name = "cmd9";
+            this.cmd9.Size = new System.Drawing.Size(43, 36);
+            this.cmd9.TabIndex = 11;
+            this.cmd9.Text = "9";
+            this.cmd9.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(221, 223);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(43, 36);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "+";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(281, 223);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(43, 36);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "-";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(221, 278);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(43, 36);
+            this.button11.TabIndex = 14;
+            this.button11.Text = "/";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(281, 278);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(43, 36);
+            this.button12.TabIndex = 15;
+            this.button12.Text = "*";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(218, 160);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(82, 13);
+            this.lblResult.TabIndex = 16;
+            this.lblResult.Text = "Résultat (00,00)";
+            // 
+            // lblNbEntier1
+            // 
+            this.lblNbEntier1.AutoSize = true;
+            this.lblNbEntier1.Location = new System.Drawing.Point(97, 70);
+            this.lblNbEntier1.Name = "lblNbEntier1";
+            this.lblNbEntier1.Size = new System.Drawing.Size(82, 13);
+            this.lblNbEntier1.TabIndex = 17;
+            this.lblNbEntier1.Text = "Nombre entier 1";
+            // 
+            // lblNbEntier2
+            // 
+            this.lblNbEntier2.AutoSize = true;
+            this.lblNbEntier2.Location = new System.Drawing.Point(97, 101);
+            this.lblNbEntier2.Name = "lblNbEntier2";
+            this.lblNbEntier2.Size = new System.Drawing.Size(82, 13);
+            this.lblNbEntier2.TabIndex = 18;
+            this.lblNbEntier2.Text = "Nombre entier 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 330);
+            this.Controls.Add(this.lblNbEntier2);
+            this.Controls.Add(this.lblNbEntier1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.cmd9);
+            this.Controls.Add(this.cmd8);
+            this.Controls.Add(this.cmd7);
+            this.Controls.Add(this.cmd6);
+            this.Controls.Add(this.cmd5);
+            this.Controls.Add(this.cmd4);
+            this.Controls.Add(this.cmd3);
+            this.Controls.Add(this.cmd2);
+            this.Controls.Add(this.cmd1);
+            this.Controls.Add(this.lblMachineACalculerInfo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblMachineACalculerInfo;
+        private System.Windows.Forms.Button cmd1;
+        private System.Windows.Forms.Button cmd2;
+        private System.Windows.Forms.Button cmd3;
+        private System.Windows.Forms.Button cmd4;
+        private System.Windows.Forms.Button cmd5;
+        private System.Windows.Forms.Button cmd6;
+        private System.Windows.Forms.Button cmd7;
+        private System.Windows.Forms.Button cmd8;
+        private System.Windows.Forms.Button cmd9;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblNbEntier1;
+        private System.Windows.Forms.Label lblNbEntier2;
+    }
+}
+
---
+++ 13-MachineACalculer-Implementation/13-MachineACalculer-Implementation/Form1.cs
@@ -0,0 +1,45 @@
+﻿//Programme: Implémentation de Machine à calculer, exo documenté par Benoit Pierrehumbert à partir de l'exo sur Moodle du même nom.
+//Samuel Roland
+//juin 2019
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13_MachineACalculer_Implementation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void number_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            int firstnb;
+            int secondnb;
+            bool numnb = true; //numéro du nb entré. si true => l
+            //Le texte du bouton est la valeur qu'il faut prendre, donc il faut le convertir en int:
+            //Selon le numnb:
+            if (numnb == true)
+            {
+                firstnb = int.Parse(btn.Text);
+            }
+            else
+            {
+                secondnb = int.Parse(btn.Text);
+            }
+            MessageBox.Show("first nb est " + firstnb.ToString() + "second nb est " + secondnb.ToString());
+
+        }
+    }
+}
