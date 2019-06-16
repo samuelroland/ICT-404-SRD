eb1798dedea39672c48087918c9167defec138ff
samuelroland <samuel.roland@cpnv.ch>
2019-06-16 13:14:38 +0200

    import et terminé 10-Debugger

---
+++ 10-Debugger (CRC) - A corriger/Debugger (CRC)/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Debugger__CRC_
+{
+    partial class frmCRC
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSource = new System.Windows.Forms.Label();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.cmdCheckCRC = new System.Windows.Forms.Button();
+            this.rbtVerif = new System.Windows.Forms.RadioButton();
+            this.rbtCalcul = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSource.Location = new System.Drawing.Point(12, 9);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(147, 20);
+            this.lblSource.TabIndex = 0;
+            this.lblSource.Text = "Numéro de carte:";
+            // 
+            // txtData
+            // 
+            this.txtData.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtData.Location = new System.Drawing.Point(15, 35);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(281, 29);
+            this.txtData.TabIndex = 3;
+            this.txtData.Text = "0000-0000-0000-0000-0000";
+            // 
+            // cmdCheckCRC
+            // 
+            this.cmdCheckCRC.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdCheckCRC.Location = new System.Drawing.Point(135, 94);
+            this.cmdCheckCRC.Name = "cmdCheckCRC";
+            this.cmdCheckCRC.Size = new System.Drawing.Size(83, 23);
+            this.cmdCheckCRC.TabIndex = 6;
+            this.cmdCheckCRC.Text = "Effectuer";
+            this.cmdCheckCRC.UseVisualStyleBackColor = true;
+            this.cmdCheckCRC.Click += new System.EventHandler(this.cmdCheckCRC_Click);
+            // 
+            // rbtVerif
+            // 
+            this.rbtVerif.AutoSize = true;
+            this.rbtVerif.Checked = true;
+            this.rbtVerif.Location = new System.Drawing.Point(16, 87);
+            this.rbtVerif.Name = "rbtVerif";
+            this.rbtVerif.Size = new System.Drawing.Size(77, 17);
+            this.rbtVerif.TabIndex = 7;
+            this.rbtVerif.TabStop = true;
+            this.rbtVerif.Text = "Vérification";
+            this.rbtVerif.UseVisualStyleBackColor = true;
+            // 
+            // rbtCalcul
+            // 
+            this.rbtCalcul.AutoSize = true;
+            this.rbtCalcul.Location = new System.Drawing.Point(16, 110);
+            this.rbtCalcul.Name = "rbtCalcul";
+            this.rbtCalcul.Size = new System.Drawing.Size(54, 17);
+            this.rbtCalcul.TabIndex = 8;
+            this.rbtCalcul.TabStop = true;
+            this.rbtCalcul.Text = "Calcul";
+            this.rbtCalcul.UseVisualStyleBackColor = true;
+            // 
+            // frmCRC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 138);
+            this.Controls.Add(this.rbtCalcul);
+            this.Controls.Add(this.rbtVerif);
+            this.Controls.Add(this.cmdCheckCRC);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.lblSource);
+            this.Name = "frmCRC";
+            this.Text = "Vérificateur de Numéro de Carte de Crédit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Button cmdCheckCRC;
+        private System.Windows.Forms.RadioButton rbtVerif;
+        private System.Windows.Forms.RadioButton rbtCalcul;
+    }
+}
+
---
+++ 10-Debugger (CRC) - A corriger/Debugger (CRC)/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+// Programme: Calcul de CRC sur un numéro de carte de crédit
+// Auteur:    X. Carrel
+// Date:      Mars 2013
+
+//Correction de 3 bugs (exo 10-Debugger): Samuel Roland
+//Date: 16.06.2019
+
+namespace Debugger__CRC_
+{
+    public partial class frmCRC : Form
+    {
+        const int NbChiffresBase = 18;    // Le nombre de chiffres dans le numéro sans CRC
+        long CRCVal = 0; // CRC (cumul)
+
+        public frmCRC()
+        {
+            InitializeComponent();
+        }
+
+        private long CRC(string Donnée)
+        // Méthode qui calcule le CRC d'une donnée.
+        {
+            int     NbChiffres = 0; // Pour compter le nombre de chiffres dans le numéro fourni
+            //ERREUR: Oublié de remettre le CRCVal à 0. donc on continue avec la somme d'avant. rajouter CRCVal=0
+            CRCVal = 0;
+
+            // Parcourir la donnée
+            for (int i = 0; i < Donnée.Length; i++)
+            {
+                char c = Donnée[i];
+                //if ((c > '0') && (c < '9')) // C'est un chiffre
+                //ERREUR: Cette condition n'inclut pas le 0 ni le 9, il faut mettre des >= et des <= donc dès qu'il y a 0 ou 9 il ne les prend pas et ce n'est donc pas valide.
+                if ((c >= '0') && (c <= '9'))
+                {
+                    NbChiffres++;
+                    CRCVal = CRCVal + ((int)c - (int)'0');
+
+                    //ERREUR: comme dessus on n'inclut pas 100 donc si CRCVal vaut 100 ca ne fonctionne pas. on a donc 100 pas égal à 0. donc on l'inclut avec >=
+                    //if (CRCVal > 100) // On ne peut pas dépasser 100 parce qu'on n'a que deux chiffres pour le CRC
+                    if (CRCVal >= 100)
+                        CRCVal = CRCVal - 100;
+                }
+            }
+            if (NbChiffres != NbChiffresBase)
+            {
+                MessageBox.Show(string.Format("Erreur: un numéro de carte doit contenir {0} chiffres (sans le CRC)",NbChiffresBase));
+                return -1;
+            }
+            return CRCVal;
+        }
+
+        private void cmdCheckCRC_Click(object sender, EventArgs e)
+        {
+            string Num = txtData.Text;
+
+            if (Num == "")
+            {
+                MessageBox.Show("Introduisez un numéro de carte SVP");
+                return;
+            }
+
+            long CRCVal;
+
+            if (rbtCalcul.Checked) // On cherche à calculer un CRC
+            {
+                CRCVal = CRC(Num); // Calcul du CRC
+                if (CRCVal >= 0)
+                    MessageBox.Show("Le CRC vaut: " + CRCVal.ToString());
+            }
+            else // On vérifie un numéro complet
+            {
+                int CRCIntro = 10 * ((int)Num[Num.Length - 2] - (int)'0') + ((int)Num[Num.Length - 1] - (int)'0'); // Le CRC inclus dans le numéro (deux derniers chiffres)
+                Num = Num.Substring(0, Num.Length - 2); // On enlève les deux derniers chiffres
+                CRCVal = CRC(Num); // et on calcule le CRC
+
+                if (CRCIntro == CRCVal)
+                    MessageBox.Show("Le numéro est valide");
+                else
+                    MessageBox.Show("Le numéro n'est pas valide");
+            }
+        }
+
+    }
+}
